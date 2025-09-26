55c998d8369dad996bf98a25e5f1a4b65621b9bf
Firsoova08 <foxalishca@mail.ru>
2025-09-26 19:08:58 +0300

    Add project files.

---
+++ Firsova3.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Firsova3._4
+{
+    enum TrafficLight { Red, Yellow, Green }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ДЗ Lab3
+            //1
+            //TrafficLight current = TrafficLight.Green;
+            //TrafficLight next;
+            //if (current == TrafficLight.Green)
+            //    next = TrafficLight.Red;
+            //else if (current == TrafficLight.Red)
+            //    next = TrafficLight.Yellow;
+            //else
+            //    next = TrafficLight.Green;
+            //Console.WriteLine($"После {current} будет {next}");
+            
+        }
+    }
+}
